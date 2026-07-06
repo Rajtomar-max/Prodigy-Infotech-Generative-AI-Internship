b83a931a1e2b36049e63815c33d670459f9333a1
Rajtomar-max <sudhatomar964@gmail.com>
2026-07-06 15:28:04 +0530

    Update main.py
    Added main.py for Task-02 Image Generation

--- Task-02-Image-Generation/main.py
+++ Task-02-Image-Generation/main.py
@@ -1,1 +1,30 @@
-print("Task 02 - Image Generation")
+import os
+from diffusers import StableDiffusionPipeline
+import torch
+
+print("="*60)
+print("🖼️ IMAGE GENERATION USING STABLE DIFFUSION")
+print("="*60)
+
+# Output folder
+os.makedirs("outputs", exist_ok=True)
+
+# Load Model
+pipe = StableDiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    torch_dtype=torch.float16
+)
+
+pipe = pipe.to("cuda")
+
+# User Prompt
+prompt = input("Enter your prompt : ")
+
+print("\nGenerating image...")
+image = pipe(prompt).images[0]
+
+# Save Image
+image.save("outputs/generated_image.png")
+
+print("\n✅ Image Generated Successfully!")
+print("Saved in outputs/generated_image.png")
